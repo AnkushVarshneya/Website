1cccd4d4941e5e4a4629332964d285c8cd746d04
Ankush Varshneya <Ankush Varshneya>
2016-01-25 14:24:16 -0500

    nested skills

--- wp-content/themes/veecard/admin/meta-box-class/meta-box-setup.php
+++ wp-content/themes/veecard/admin/meta-box-class/meta-box-setup.php
@@ -104,11 +104,10 @@ if (is_admin()){
   $my_meta3->Finish();
 
 
-
-  //Skills info meta box config
+  //Skills type info meta box config
 	$config4 = array(
-    'id' => 'skills_info',          		// meta box id, unique per meta box
-    'title' => 'Skills',          			// meta box title
+    'id' => 'skills_type_info',          		// meta box id, unique per meta box
+    'title' => 'Skills Type',          			// meta box title
 	  'pages' => array('resume'),      		// post types, accept custom post types as well, default is array('post'); optional
     'context' => 'normal',            		// where the meta box appear: normal (default), advanced, side; optional
     'priority' => 'high',            		// order of meta box: high (default), low; optional
@@ -121,16 +120,46 @@ if (is_admin()){
   //Initiate skills info meta box
   $my_meta4 =  new AT_Meta_Box($config4);
 
+  
+
+	//Skills info meta box config
+		$config41 = array(
+		'id' => 'skills_info',          		// meta box id, unique per meta box
+		'title' => 'Skills',          			// meta box title
+		  'pages' => array('resume'),      		// post types, accept custom post types as well, default is array('post'); optional
+		'context' => 'normal',            		// where the meta box appear: normal (default), advanced, side; optional
+		'priority' => 'high',            		// order of meta box: high (default), low; optional
+		'fields' => array(),            		// list of meta fields (can be added by field arrays)
+		'local_images' => true,          		// Use local or hosted images (meta box images for add/remove)
+		'use_with_theme' => true          		//change path if used with theme set to true, false for a plugin or anything else for a custom path(default false).
+	  );
+
+
+	  //Initiate skills info meta box
+	  $my_meta41 =  new AT_Meta_Box($config41);
+
+	  //Skills info fields
+	  $repeater_fields41[] = $my_meta41->addText($prefix.'skill_name',array('name'=> 'Skill Name '),true);
+	  $repeater_fields41[] = $my_meta41->addSelect($prefix.'skill_level',array('1'=>'1','2'=>'2','3'=>'3','41'=>'41','5'=>'5','6'=>'6','7'=>'7','8'=>'8','9'=>'9','10'=>'10'),array('name'=> 'Skill Level ', 'std'=> array('2')),true);
+
+	  //Skills repeater block
+	  $my_meta41->addRepeaterBlock($prefix.'skill_',array('inline' => true, 'name' => 'Enter your skill info','fields' => $repeater_fields41, 'sortable'=> true));
+
+
+	  //Finish skills info meta mox decleration
+	  $my_meta41->Finish();
+	  
   //Skills info fields
-  $repeater_fields4[] = $my_meta4->addText($prefix.'skill_name',array('name'=> 'Skill Name '),true);
-  $repeater_fields4[] = $my_meta4->addSelect($prefix.'skill_level',array('1'=>'1','2'=>'2','3'=>'3','4'=>'4','5'=>'5','6'=>'6','7'=>'7','8'=>'8','9'=>'9','10'=>'10'),array('name'=> 'Skill Level ', 'std'=> array('2')),true);
+  $repeater_fields4[] = $my_meta4->addText($prefix.'skill_type_name',array('name'=> 'Skill Name '),true);
+  $repeater_fields4[] = $my_meta4->addField($prefix.'skills', $my_meta41);
 
   //Skills repeater block
-  $my_meta4->addRepeaterBlock($prefix.'skill_',array('inline' => true, 'name' => 'Enter your skill info','fields' => $repeater_fields4, 'sortable'=> true));
+  $my_meta4->addRepeaterBlock($prefix.'skill_type_',array('inline' => true, 'name' => 'Enter your skill type info','fields' => $repeater_fields4, 'sortable'=> true));
 
 
   //Finish skills info meta mox decleration
-  $my_meta4->Finish();
+  $my_meta4->Finish(); 
+
   
    //Volunteering info meta box config
   $config5 = array(
